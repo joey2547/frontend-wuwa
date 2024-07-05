6ca5e0c9c2d7bcbff9088c5a678fa39998ee1140
joey2547 <rapeepat.ton@student.mahidol.edu>
2024-07-06 00:09:48 +0700

    new index joey

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import SignIn from '../components/SignIn.vue'
+import SignUp from '../components/SignUp.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -6,6 +8,16 @@ const router = createRouter({
     {
       path: '/',
       name: 'home'
+    },
+    {
+      path: '/signin',
+      name: 'SignIn',
+      component: SignIn
+    },
+    {
+      path: '/signup',
+      name: 'SignUp',
+      component: SignUp
     }
   ]
 })
