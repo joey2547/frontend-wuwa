d14f4836cc909b58ebd2157513092ae2a00bd2d4
joey2547 <rapeepat.ton@student.mahidol.edu>
2024-07-06 00:11:02 +0700

    frontend app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue'
+import SignIn from './components/SignIn.vue'
+import SignUp from './components/SignUp.vue'
 </script>
 
 <template>
-  <NavBar></NavBar>
+  <v-app>
+    <NavBar></NavBar>
+    <RouterView></RouterView> <!-- To handle route views if you have a router setup -->
+    
+  </v-app>
 </template>
 
 <style scoped>
-
+/* Add any scoped styles here if needed */
 </style>
